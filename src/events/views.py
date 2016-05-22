5e235dc9b8b6a44c327ce5664a900af1b47ff0be
Daniel Quinn <code@danielquinn.org>
2016-05-22 13:27:58 +0100

    -

--- src/events/views.py
+++ src/events/views.py
@@ -1,5 +1,7 @@
 import json
 
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
 from .models import Selector, Event
@@ -25,7 +27,7 @@ class DashboardView(TemplateView):
 
         context["geography"] = self.get_geojson()
         context["log"] = self.get_connection_log()
-        print(context["log"])
+        print(json.dumps(json.loads(context["log"]), indent=2))
 
         return context
 
@@ -42,10 +44,21 @@ class DashboardView(TemplateView):
         return json.dumps(r, separators=(",", ":"))
 
     def get_connection_log(self):
-        return json.dumps(
-            Event.get_raw_data(
-                sorted(list(self.probes), key=lambda x: x["timestamp"])[:300]
-            ),
-            separators=(",", ":"),
-            indent=2
+
+        r = {
+            "x": [],
+            "connect": [],
+            "disconnect": []
+        }
+
+        events = sorted(Event.get_raw_data(
+            list(self.probes)[:300]),
+            key=lambda x: x["timestamp"]
         )
+
+        for delta in events:
+            bucket = delta["timestamp"] // 15
+            t = bucket * 15
+
+            r["x"].append(datetime.fromtimestamp(bucket).strftime("%Y-%m-%d %H:%i%s"))
+            r[delta["event"]] = 7
