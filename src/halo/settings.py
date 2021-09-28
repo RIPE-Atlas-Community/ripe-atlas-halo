872e90932a36365826743b6ceb76f96bba4f1a24
Dante Lanznaster <dantecl@gmail.com>
2021-09-27 22:37:07 -0700

    Changed middleware var for compatibility reasons

--- src/halo/settings.py
+++ src/halo/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
